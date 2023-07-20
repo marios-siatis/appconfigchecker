4b69e251b8dff7cbf96085b0300bcdba08640430
Marios Siati <marios.siati@kpmg.co.uk>
2023-07-20 17:17:51 +0100

    Initial

---
+++ AppConfigChecker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace AppConfigChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // replace with connection string 
+            const string connectionString = "";
+            // replace with an appconfig
+            const string mySetting = "Api/ChatApp:ClientId";
+            
+            IConfiguration config = new ConfigurationBuilder()
+                .AddAzureAppConfiguration(options =>
+                {
+                    options.Connect(connectionString)
+                        .UseFeatureFlags();
+                })
+                .Build();
+
+            var settingValue = config[mySetting];
+            Console.WriteLine($"Setting {mySetting}: {settingValue}");
+        }
+    }
+}
